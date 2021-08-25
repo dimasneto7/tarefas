0d262d492da47533b3195a17d1cbf4b8f98ad486
Dimas Neto <dimas_fcn@outlook.com>
2021-08-25 09:53:20 -0300

    first

---
+++ src/pages/index.tsx
@@ -0,0 +1,15 @@
+export default function Home() {
+  return (
+    <div
+      className={`
+      flex flex-col justify-center items-center h-screen
+      bg-gradient-to-tr from-blue-500 to-purple-900
+      text-white
+    `}
+    >
+      <span>1</span>
+      <span>2</span>
+      <span>3</span>
+    </div>
+  );
+}
